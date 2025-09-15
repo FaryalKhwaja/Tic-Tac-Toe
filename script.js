c477f4f57e9e09198ab8e996783c801ae42db440
FaryalKhwaja <iamfaryalkhwaja@gmail.com>
2025-09-15 16:43:43 +0500

    Add 'declare winner' function

--- script.js
+++ script.js
@@ -2,9 +2,11 @@ let cells = document.querySelectorAll(".cell");
 let turnDisplay = document.querySelector(".turn");
 
 let currentPlayer = "X";
+let gameOver = false;
 
 cells.forEach(cell => {
     cell.addEventListener("click", () => {
+        if (gameOver) return;
         if (cell.textContent !== "") return;
         if (cell.textContent === "") {
             cell.textContent = currentPlayer;
@@ -15,6 +17,7 @@ cells.forEach(cell => {
             currentPlayer = "X";
         }
         turnDisplay.textContent = `${currentPlayer} Turn`; 
+        checkWinner();
     });
 });
 
@@ -24,3 +27,23 @@ const winPatterns = [
   [0,4,8], [2,4,6] 
 ];
 
+function checkWinner () {
+    for (let i = 0; i < winPatterns.length; i++) {
+        let patterns = winPatterns[i];
+        let [a, b, c] = patterns;
+        let cellA = cells[a].textContent;
+        let cellB = cells[b].textContent;
+        let cellC = cells[c].textContent;
+        if (cellA && cellA === cellB && cellA === cellC) {
+            turnDisplay.textContent = `${cellA} wins!`;
+            gameOver = true;
+            return;
+        }
+        if (Array.from(cells).every(function (cell) {
+            return cell.textContent;
+        })) {
+            turnDisplay.textContent = `It's a draw!`;
+            gameOver = true;
+        }
+    }
+}
